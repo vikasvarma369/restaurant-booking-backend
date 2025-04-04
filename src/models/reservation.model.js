0f6cf5285e4a08279c72b900d7ccc2f69a9884ed
Vikas singh varma <Vikashverma3001@gmail.com>
2025-04-04 14:39:48 +0530

    add : create reservation model schema for restaurant, user, date, time, number of guests, and status

---
+++ src/models/reservation.model.js
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+
+
+const reservationSchema = new Schema({
+  restaurant: {
+    type: Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  numberOfGuests: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  Status: {
+    type: String,
+    enum: ['confirmed', 'cancelled'],
+    default: 'confirmed',
+  }
+}, {
+  timestamps: true
+});
+
+
+const Reservation = model("Reservation", reservationSchema);
+export default Reservation;
